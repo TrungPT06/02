9df07c2e75f7482de2aefea763413c657f686193
TrunPT <phtrun23506@gmail.com>
2025-10-22 13:27:06 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import "./styles.css";
+//@ts-ignore
+import Home from "./Home";
+//@ts-ignore
+import Layout from "./Layout";
+//@ts-ignore
+import Trang1 from "./Trang1";
+// @ts-ignore
+import Trang2 from "./Trang2";
+
+//@ts-ignore
+import Chitietsanpham from "./Chitietsanpham";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  // return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="trang1" element={<Trang1 />} />
+          <Route path="trang2" element={<Trang2 />} />
+          <Route path="sanpham/:id" element={<Chitietsanpham />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
